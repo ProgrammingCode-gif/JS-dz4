e6ad84c2d8ea877b2f84c1bbc50c040242a7918b
Shahzod <shahzodtashev49@gmail.com>
2022-05-09 22:05:58 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var a = prompt('Введите первое число');
+var b = prompt('Введите второе число');
+var c = prompt('Введите третье число');
+
+if(a>b && b>c || c>b && b>a){
+    alert(b + ' - среднее число')
+}else if(b>a && a>c || c>a && a>b){
+    alert(a + ' - среднее число')
+}else if(a>c && c>b || b>c && c>a){
+    alert(c + ' - среднее число')
+}
